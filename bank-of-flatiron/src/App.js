f62dd5c07318edfd63bafe4fc6e694f8ce86ac5e
arnold <arnoldkmaina@gmail.com>
2024-05-02 16:20:56 +0300

    bank-of-flatiron

---
+++ bank-of-flatiron/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import AccountContainer from "./components/AccountContainer";
+
+function App() {
+  return (
+    <div className="ui raised segment">
+      <div className="ui segment green inverted">
+        <h2>The Royal Bank of Flatiron</h2>
+      </div>
+      <AccountContainer />
+
+
+    </div>
+  );
+}
+
+export default App;
+     
+        
+      
+      
+
+  
+  
+
+
